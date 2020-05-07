5caf11a325a93d33610af525403c58680b801ec3
yaizuuuu <p.i.o.y.m.a.a@gmail.com>
2020-05-07 19:05:34 +0900

    実装

--- lib/cdk-handson-stack.ts
+++ lib/cdk-handson-stack.ts
@@ -6,7 +6,7 @@ export class CdkHandsonStack extends cdk.Stack {
   constructor (scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    const { vpc } = new Vpc(this)
+    const { vpc } = new Vpc(this).forApi()
     new SecurityGroup(this).forInternal(vpc)
     new SecurityGroup(this).forWebAny(vpc)
   }
