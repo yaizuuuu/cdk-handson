f53bf5bf2e87cbf3c64a64ee4862f715aa56773f
yaizuuuu <p.i.o.y.m.a.a@gmail.com>
2020-05-07 19:25:26 +0900

    実装

--- test/cdk-handson.test.ts
+++ test/cdk-handson.test.ts
@@ -2,20 +2,16 @@ import { expect as expectCDK, haveResource } from '@aws-cdk/assert'
 import * as cdk from '@aws-cdk/core'
 import * as CdkHandson from '../lib/cdk-handson-stack'
 
-test('SQS Queue Created', () => {
-  const app = new cdk.App()
+test('VPC Created', () => {
+  const app = new cdk.App({
+    context: {
+      env: 'dev'
+    }
+  })
   // WHEN
   const stack = new CdkHandson.CdkHandsonStack(app, 'MyTestStack')
   // THEN
-  expectCDK(stack).to(haveResource('AWS::SQS::Queue', {
-    VisibilityTimeout: 300
+  expectCDK(stack).to(haveResource('AWS::EC2::VPC', {
+    CidrBlock: '192.168.2.0/24'
   }))
-})
-
-test('SNS Topic Created', () => {
-  const app = new cdk.App()
-  // WHEN
-  const stack = new CdkHandson.CdkHandsonStack(app, 'MyTestStack')
-  // THEN
-  expectCDK(stack).to(haveResource('AWS::SNS::Topic'))
 })
